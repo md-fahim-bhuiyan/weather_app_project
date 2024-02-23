312649db451d4677449f0502f325942489cb5af3
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2024-02-23 20:49:04 +0600

    first commit

---
+++ weather_app/urls.py
@@ -0,0 +1,7 @@
+# weather_app/urls.py
+from django.urls import path
+from .views import weather_home
+
+urlpatterns = [
+    path('', weather_home, name='weather_home'),
+]
---
+++ weather_app/views.py
@@ -0,0 +1,27 @@
+# weather_app/views.py
+from django.shortcuts import render
+import requests
+
+def get_weather(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric',  # You can change units to imperial for Fahrenheit
+    }
+    response = requests.get(base_url, params=params)
+    weather_data = response.json()
+    return weather_data
+
+def weather_home(request):
+    api_key = 'de1f124bf2344600239ce77ed33c4f75'
+    city = request.GET.get('city', 'Dhaka')  # Default city is Dhaka
+
+    weather_data = get_weather(api_key, city)
+
+    context = {
+        'city': city,
+        'weather_data': weather_data,
+    }
+
+    return render(request, 'weather_home.html',context)
